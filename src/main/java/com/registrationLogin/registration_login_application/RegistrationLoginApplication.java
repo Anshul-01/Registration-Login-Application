d48f251cb119b180883d1a678973d6a82dc94e97
Anshul <anshulverma1299@gmail.com>
2024-10-06 00:14:16 +0530

    First Commit Set up MySQL

---
+++ src/main/java/com/registrationLogin/registration_login_application/RegistrationLoginApplication.java
@@ -0,0 +1,13 @@
+package com.registrationLogin.registration_login_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registrationLogin/registration_login_application/RegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registrationLogin.registration_login_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
